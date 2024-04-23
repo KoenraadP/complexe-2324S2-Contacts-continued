d09afd12b7fa651d3ea7b3754e3b10bafa74c172
Koenraad Pecceu <koenraad.pecceu@gmail.com>
2024-04-23 13:45:24 +0200

    Remove crucial code elements :)

--- Contacts/frmContacts.cs
+++ Contacts/frmContacts.cs
@@ -16,11 +16,40 @@ namespace Contacts
         public frmContacts()
         {
             InitializeComponent();
+            // standaard bestandinlezen, pas de methode aan als het bestand niet klopt
+            // of comment de lijn als je niet van plan bent te werken met een standaard bestand
+            DefaultFile();
+        }
 
-            // testbestand automatisch al inlezen
+        // methode om standaard bestand in te lezen bij opstarten
+        // indien niet aanwezig dan wordt dit bestand aangemaakt in deze methode
+        private void DefaultFile()
+        {
             // @ nodig om backslashes toe te laten
+            string directoryPath = @"c:\contacten\";
             fullPath = @"c:\contacten\creo.txt";
+
+            // controleren of bestand al bestaat
+            if (!File.Exists(fullPath))
+            {
+                // controleren of map al bestaat
+                if (!Directory.Exists(directoryPath))
+                {
+                    Directory.CreateDirectory(directoryPath);
+                }
+
+                // inhoud voor standaard bestand als dit nog niet bestond
+                string fileContents = $"1;Koenraad;Pecceu;koenraad.pecceu@creo.be\r\n" +
+                    $"2;Sam;Vaneeckhoutte;sam.vaneeckhoutte@creo.be\r\n" +
+                    $"3;Mieke;Lapeire;mieke.lapeire@creo.be\r\n" +
+                    $"4;Jelle;Vyncke;jelle.vyncke@creo.be";
+
+                File.WriteAllText(fullPath, fileContents);
+            }
+
+            // personen uit bestand inlezen
             AddPeople(fullPath);
+            // eerste persoon in combobox selecteren
             cbPeople.SelectedIndex = 0;
         }
 
@@ -33,7 +62,7 @@ namespace Contacts
             // fullPath = ofd.FileName;
 
             // alle regels uit tekstbestand in array linesFromFile stoppen
-            linesFromFile = File.ReadAllLines(fullPath);
+            linesFromFile = File.;
 
             // testen wat het eerste en tweede element
             // in de array linesFromFile is
@@ -42,27 +71,27 @@ namespace Contacts
 
             // linesFromFile doorlopen en ieder lijntje splitsen
             // daarna personen aanmaken en waarden toekennen
-            foreach (var line in linesFromFile)
+            foreach (var line in )
             {
                 // één line is bvb
                 // 1;Koenraad;Pecceu;koenraad.pecceu@creo.be
                 // ik moet deze line splitsen op ';'
                 // de data komt nu in een nieuwe array terecht
-                string[] personInfo = line.Split(';');
+                string[] personInfo = line.
                 // testen of de namen correct er in komen
                 Debug.WriteLine(personInfo[1]);
 
                 // nieuwe person variabele/object maken
-                Person p = new Person();
+                Person p = new ;
                 // data vanuit personInfo array gebruiken
                 // om properties van p in te vullen
-                p.Id = Convert.ToInt32(personInfo[0]);
+                p.Id = ;
                 p.FirstName = personInfo[1];
                 p.LastName = personInfo[2];
                 p.Email = personInfo[3];
 
                 // persoon p toevoegen aan combobox
-                cbPeople.Items.Add(p);
+                cbPeople.;
             }
 
         }
@@ -72,7 +101,7 @@ namespace Contacts
         private void UpdateFile()
         {
             // geselecteerde persoon omzetten naar Person
-            Person p = (Person)cbPeople.SelectedItem;
+            Person p = 
 
             // alle waarden van p opnieuw invullen
             // op basis van waarden die in textboxes staan
@@ -85,7 +114,7 @@ namespace Contacts
             // index opzoeken van persoon
             // dit om te zorgen dat we de juiste waarde
             // op de juiste plaats in de array kunnen aanpassen
-            int index = cbPeople.SelectedIndex;
+            int index = 
 
             // lijn van persoon in lines array vervangen door
             // nieuwe waarden
